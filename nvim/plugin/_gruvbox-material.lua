9c668d205e000d63f0624e1634039ec6a3a0f6d7
C-Rmoser <rohrmoser.christoph91@gmail.com>
2024-03-12 13:11:37 +0100

    Adjust lsp error and warning colors

--- nvim/plugin/_gruvbox-material.lua
+++ nvim/plugin/_gruvbox-material.lua
@@ -1,3 +1,5 @@
 vim.o.background = 'dark'
 vim.cmd.colorscheme("gruvbox-material")
 
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { link = "Error" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { link = "WarningMsg" })
