494c025093c0cc3b3dddd22d18548cec26b2b8f1
C-Rmoser <rohrmoser.christoph91@gmail.com>
2024-03-26 13:52:36 +0100

    add cargo directory to path

--- home.nix
+++ home.nix
@@ -109,6 +109,7 @@
       if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
         exec tmux new-session -A -s default
       fi
+      export PATH="$HOME/.cargo/bin:$PATH"
     '';
   };
 
